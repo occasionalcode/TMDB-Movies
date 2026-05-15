8ead5955bf51f60118f84f9d93897c3baecca962
Gabriel I. <occasionalprogrammer@gmail.com>
2026-05-15 18:50:19 +0800

    chore, updated responsiveneses of tv categories

--- src/component/tv/-TVCategories.tsx
+++ src/component/tv/-TVCategories.tsx
@@ -27,7 +27,7 @@ export function TVCategories({ category, shows }: TVCategoryTypes) {
               {shows.results.map((show) => (
                 <CarouselItem
                   key={show.id}
-                  className="basis-1/2 mobileL:basis-1/3 sm:basis-1/4 md:basis-1/5 lg:basis-1/7"
+                  className="basis-1/2  sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/7"
                 >
                   <TVCards show={show} />
                 </CarouselItem>
